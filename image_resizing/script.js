78decf596107751d49ea14ebcc7b64fecfe2d934
sadaf_sayyed <sadafsayyed49@gmail.com>
2025-11-27 20:41:02 +0530

    image_rsizing

---
+++ image_resizing/script.js
@@ -0,0 +1,77 @@
+let imageElement = document.getElementById("image");
+
+let imageWidthElement = document.getElementById("imageWidth");
+
+let warningMessageElement = document.getElementById("warningMessage");
+
+
+
+
+
+let maxImageWidth = 300;
+
+let minImageWidth = 100;
+
+let defaultImageWidth = 200;
+
+let maxWidthWarningMessage = "Too big. Decrease the size of the Image.";
+
+let minWidthWarningMessage = "Ain't visible. Increase the size of the Image.";
+
+
+
+imageElement.style.width = defaultImageWidth + "px";
+
+imageWidthElement.textContent = defaultImageWidth + "px";
+
+
+
+function onIncrement() {
+
+    if (defaultImageWidth >= maxImageWidth) {
+
+        warningMessageElement.textContent = maxWidthWarningMessage;
+
+    } else {
+
+        defaultImageWidth = defaultImageWidth + 5;
+
+        let updatedImageWidth = defaultImageWidth + "px";
+
+
+
+        warningMessageElement.textContent = "";
+
+        imageElement.style.width = updatedImageWidth;
+
+        imageWidthElement.textContent = updatedImageWidth;
+
+    }
+
+}
+
+
+
+function onDecrement() {
+
+    if (defaultImageWidth <= minImageWidth) {
+
+        warningMessageElement.textContent = minWidthWarningMessage;
+
+    } else {
+
+        defaultImageWidth = defaultImageWidth - 5;
+
+        let updatedImageWidth = defaultImageWidth + "px";
+
+
+
+        warningMessageElement.textContent = "";
+
+        imageElement.style.width = updatedImageWidth;
+
+        imageWidthElement.textContent = updatedImageWidth;
+
+    }
+
+}
